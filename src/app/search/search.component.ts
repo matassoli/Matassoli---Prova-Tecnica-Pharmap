c644ed42e36f2d5077b0c45cf8e5c4a114b57726
Nicolò Matassoli <nico.matassoli@gmail.com>
2022-05-25 01:39:48 +0200

    add: css card component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -11,20 +11,35 @@ export class SearchComponent implements OnInit {
   constructor(private openFoodService: OpenFoodService) {}
 
   public foodList: any;
-  public selected: any;
+  public selected = '';
+  public selectedData: any;
 
   ngOnInit(): void {
     this.openFoodService.getApiData().subscribe((data) => {
       this.foodList = data;
+      console.log(this.foodList);
     });
   }
 
+  // filter to remove null values from the select
   filterEmptyValues() {
-    return this.foodList.products.filter((x: any) => x.generic_name_it);
+    return this.foodList.products.filter(
+      (x: any) => x.abbreviated_product_name
+    );
   }
+
+  // recovering full products data from selected option
   selectedValue(value: any) {
-    console.log(value.$ngOptionLabel);
+    for (let i = 0; i < this.foodList.products.length; i++) {
+      if (value.$ngOptionLabel === this.foodList.products[i].abbreviated_product_name) {
+        this.selectedData = this.foodList.products[i];
+        // console.log(this.selectedData.nutriments);
+        break;
+      }
+    }
   }
+
+  // emptying search bar after clicking on it
   emptyOption() {
     this.selected = '';
   }
